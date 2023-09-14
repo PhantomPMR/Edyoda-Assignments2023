f1c32f1aac746c9c0c5b41f55038e44f8e80a114
PhantomPMR <140829907+PhantomPMR@users.noreply.github.com>
2023-09-14 22:03:15 +0530

    Update Squares1.py

--- Squares1.py
+++ Squares1.py
@@ -1,3 +1,9 @@
-Given_list = [4, 5, 2, 9]
-squared_list = list(map(lambda x: x ** 2, Given_list))
+def square_number(x):
+    return x * x
+input_str = input("Enter a list of integers separated by commas: ")
+input_list = [int(num) for num in input_str.split(',')]
+
+squared_list = list(map(square_number, input_list))
+
+print("Square the elements of the list:")
 print(squared_list)
